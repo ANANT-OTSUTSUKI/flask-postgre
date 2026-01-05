53e23a304c6782bd772db735b76ebb777649d35d
ANANT-OTSUTSUKI <anantyadav1006@gmail.com>
2026-01-05 23:29:59 +0530

    Update user.py

--- user.py
+++ user.py
@@ -1,25 +1,26 @@
-from flask import Blueprint,request,Flask,render_template,redirect,url_for
-from flask_login import login_required,current_user,login_user,logout_user,UserMixin
-from model import db,REQ,db_conn
-user = Blueprint('user',__name__,template_folder='templates')
+from flask import Blueprint, request
+from flask_login import login_required, current_user
+from model import db, REQ
+
+user = Blueprint('user', __name__, template_folder='templates')
 
 
-
-@user.route('/request-approval', methods=['post'])
+@user.route('/request-approval', methods=['POST'])
 @login_required
 def req_approval():
-    s_id = request.form['s_id']
-    req = REQ.query.filter_by(s_id=s_id).first()
-    if req:
-        return 'APPROVAL IS IN PROCESS'
-    else:
-        conn = db_conn()
-        cur=conn.cursor()
-        cur.execute('''SELECT username FROM staffs WHERE s_id=%s''',(s_id))
-        username = cur.fetchone()
-        cur.execute('''INSERT INTO req_approval(s_id,username) VALUES(%s,%s)''',(s_id,username))
-        conn.commit()
-        cur.close()
-        conn.close()
+    # Never trust form s_id, use logged-in user
+    s_id = current_user.s_id
+    username = current_user.username
 
-        return 'APPROVAL REQUESTED'
+    existing = REQ.query.filter_by(s_id=s_id).first()
+    if existing:
+        return 'APPROVAL IS IN PROCESS'
+
+    req = REQ(
+        s_id=s_id,
+        username=username
+    )
+    db.session.add(req)
+    db.session.commit()
+
+    return 'APPROVAL REQUESTED'
