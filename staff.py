c5406c6177b1f8a51f9d85743a3281405896f27b
ANANT-OTSUTSUKI <anantyadav1006@gmail.com>
2026-01-05 23:29:07 +0530

    Update staff.py

--- staff.py
+++ staff.py
@@ -1,148 +1,104 @@
-from flask import Blueprint,request,Flask,render_template,redirect,url_for
-from flask_login import login_required,current_user,login_user,logout_user,UserMixin
-from model import db,REQ,db_conn,REP,Customer,Trans
-staff = Blueprint('staff',__name__,template_folder='templates')
+from flask import Blueprint, request, render_template, redirect, url_for
+from flask_login import login_required, current_user
+from model import db, Customer, Trans, REP, STAFF
 import datetime
 
+staff = Blueprint('staff', __name__, template_folder='templates')
 
-@staff.route('/create-customer', methods=['post'])
+# =========================
+# CUSTOMER MANAGEMENT
+# =========================
+
+@staff.route('/create-customer', methods=['POST'])
 @login_required
 def create_customer():
-    name = request.form['name']
-    mail = request.form['mail']
-    address = request.form['address']
+    customer = Customer(
+        name=request.form['name'],
+        mail=request.form['mail'],
+        address=request.form['address']
+    )
+    db.session.add(customer)
+    db.session.commit()
+    return redirect(url_for('staff.staff'))
 
-    conn=db_conn()
-    cur=conn.cursor()
-    cur.execute('''INSERT INTO customers(name,mail,address) VALUES(%s,%s,%s);''',(name,mail,address))
-    conn.commit()
-    cur.close()
-    conn.close()
-    return redirect(url_for('staff'))
-@staff.route('/upc',methods=['post'])
-@login_required
-def upc():
-    c_id = request.form['c_id']
-    customer=Customer.query.filter_by(c_id=c_id).first()
-    if not customer:
-        return 'Enter Valid Customer ID'
-    conn= db_conn()
-    cur=conn.cursor()
-    cur.execute('''SELECT * FROM customers WHERE c_id=%s''',(c_id))
-    data=cur.fetchone()
-    conn.commit()
-    cur.close()
-    conn.close()
-    return render_template('upc.html',data=data)
 
-@staff.route('/update-customer',methods=['post'])
+@staff.route('/update-customer', methods=['POST'])
 @login_required
 def update_customer():
-    c_id=request.form['c_id']
-    name = request.form['name']
-    mail = request.form['mail']
-    address = request.form['address']
-    conn=db_conn()
-    cur=conn.cursor()
-    cur.execute('''UPDATE customers SET (name,mail,address)=(%s,%s,%s) WHERE c_id = %s''',(name,mail,address,c_id))
-    conn.commit()
-    cur.close()
-    conn.close()
-    return redirect(url_for('staff'))
+    customer = Customer.query.get(request.form['c_id'])
+    if customer:
+        customer.name = request.form['name']
+        customer.mail = request.form['mail']
+        customer.address = request.form['address']
+        db.session.commit()
+    return redirect(url_for('staff.staff'))
 
-@staff.route('/delete-customer',methods=['post'])
+
+@staff.route('/delete-customer', methods=['POST'])
 @login_required
 def delete_customer():
-    c_id=request.form['c_id']
-    customer=Customer.query.filter_by(c_id=c_id).first()
-    if not customer:
-        return 'Enter Valid Customer ID'
-    conn=db_conn()
-    cur=conn.cursor()
-    cur.execute('''DELETE FROM customers where c_id=%s''',(c_id))
-    conn.commit()
-    cur.close()
-    conn.close()
-    return redirect(url_for('staff'))
+    customer = Customer.query.get(request.form['c_id'])
+    if customer:
+        db.session.delete(customer)
+        db.session.commit()
+    return redirect(url_for('staff.staff'))
 
+# =========================
+# REQUEST ADMIN APPROVAL
+# =========================
 
-@staff.route('/request-approval', methods=['post'])
+@staff.route('/request-approval', methods=['POST'])
 @login_required
 def req_admin():
-    s_id = request.form['s_id']
-    req = REP.query.filter_by(s_id=s_id).first()
-    if req:
+    existing = REP.query.filter_by(s_id=current_user.s_id).first()
+    if existing:
         return 'APPROVAL IS ALREADY IN PROCESS'
-    else:
-        conn = db_conn()
-        cur=conn.cursor()
-        cur.execute('''SELECT username FROM staffs WHERE s_id=%s''',(s_id))
-        username = cur.fetchone()
-        cur.execute('''INSERT INTO req_admin(s_id,username) VALUES(%s,%s)''',(s_id,username))
-        conn.commit()
-        cur.close()
-        conn.close()
 
-        return 'APPROVAL REQUESTED'
-    
-@staff.route('/add-transaction', methods=['post'])
+    req = REP(
+        s_id=current_user.s_id,
+        username=current_user.username
+    )
+    db.session.add(req)
+    db.session.commit()
+    return 'APPROVAL REQUESTED'
+
+# =========================
+# TRANSACTIONS
+# =========================
+
+@staff.route('/add-transaction', methods=['POST'])
 @login_required
 def add_trans():
-    c_id=request.form['c_id']
-    s_id=request.form['s_id']
-    amount=request.form['amount']
+    trans = Trans(
+        c_id=request.form['c_id'],
+        s_id=current_user.s_id,
+        amount=request.form['amount'],
+        t_date=datetime.date.today(),
+        t_time=datetime.datetime.now().time()
+    )
+    db.session.add(trans)
+    db.session.commit()
+    return redirect(url_for('staff.staff'))
 
-    conn=db_conn()
-    cur=conn.cursor()
-    cur.execute('''INSERT INTO transactions(c_id,s_id,amount,t_date,t_time) VALUES(%s,%s,%s,%s,%s)''',(c_id,s_id,amount,datetime.date.today(),datetime.datetime.now().time()))
-    conn.commit()
-    cur.close()
-    conn.close()
-    return redirect(url_for('staff'))
-    
 
-@staff.route('/upt',methods=['post'])
-@login_required
-def upt():
-    t_id = request.form['t_id']
-    trans=Trans.query.filter_by(t_id=t_id).first()
-    if not trans:
-        return 'Enter Valid Transaction ID'
-    conn= db_conn()
-    cur=conn.cursor()
-    cur.execute('''SELECT * FROM transactions WHERE t_id=%s''',(t_id))
-    data=cur.fetchone()
-    conn.commit()
-    cur.close()
-    conn.close()
-    return render_template('upt.html',data=data)
-
-@staff.route('/update-transaction',methods=['post'])
+@staff.route('/update-transaction', methods=['POST'])
 @login_required
 def update_trans():
-    t_id=request.form['t_id']
-    s_id = request.form['s_id']
-    c_id = request.form['c_id']
-    amount = request.form['amount']
-    conn=db_conn()
-    cur=conn.cursor()
-    cur.execute('''UPDATE transactions SET (s_id,c_id,amount,t_time,t_date)=(%s,%s,%s,%s,%s) WHERE t_id = %s''',(s_id,c_id,amount,datetime.datetime.now().time(),datetime.date.today(),t_id))
-    conn.commit()
-    cur.close()
-    conn.close()
-    return redirect(url_for('staff'))
+    trans = Trans.query.get(request.form['t_id'])
+    if trans:
+        trans.c_id = request.form['c_id']
+        trans.amount = request.form['amount']
+        trans.t_date = datetime.date.today()
+        trans.t_time = datetime.datetime.now().time()
+        db.session.commit()
+    return redirect(url_for('staff.staff'))
 
-@staff.route('/delete-transaction',methods=['post'])
+
+@staff.route('/delete-transaction', methods=['POST'])
 @login_required
 def delete_trans():
-    t_id=request.form['t_id']
-    transaction=Trans.query.filter_by(t_id=t_id).first()
-    if not transaction:
-        return 'Enter Valid transaction ID'
-    conn=db_conn()
-    cur=conn.cursor()
-    cur.execute('''DELETE FROM transactions where t_id=%s''',(t_id))
-    conn.commit()
-    cur.close()
-    conn.close()
-    return redirect(url_for('staff'))
+    trans = Trans.query.get(request.form['t_id'])
+    if trans:
+        db.session.delete(trans)
+        db.session.commit()
+    return redirect(url_for('staff.staff'))
